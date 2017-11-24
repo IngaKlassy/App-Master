8622c71c3300357e8994c6a523d59d35b33ae5e8
Daniel <dlmartin@ucsd.edu>
2017-11-24 07:37:48 -0800

    added tracker code

--- js/index.js
+++ js/index.js
@@ -8,3 +8,17 @@ $(document).ready(function() {
         $(':input','#suggestTerm').val("");
     });
 });
+
+
+
+$("#termImg").click(function()
+{
+  console.log("clicked Term of Day Image");
+  tracker.send('event', 'image', 'click');
+});
+
+$("#termButton").click(function()
+{
+  console.log("clicked Term of Day Button");
+  tracker.send('event', 'image', 'click');
+});
