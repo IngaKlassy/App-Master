26d22966cb9bb08ce820ffc30fb83b86319173e3
Daniel <dlmartin@ucsd.edu>
2017-11-24 10:23:45 -0800

    minor tracking changes (testing)

--- js/index.js
+++ js/index.js
@@ -21,11 +21,11 @@ $(document).ready(function() {
 $("#termImg").click(function()
 {
   console.log("clicked Term of Day Image");
-  tracker.send('event', 'term', 'click');
+  tracker.send('event', 'jsIndex', 'click');
 });
 
 $("#termButton").click(function()
 {
   console.log("clicked Term of Day Button");
-  tracker.send('event', 'term', 'click');
+  tracker.send('event', 'jsIndex2', 'click');
 });
